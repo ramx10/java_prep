5f01deb72f6d1fa69d0a27da897446875a553198
ramx10 <Shipalkarram@gmail.com>
2026-06-18 09:21:57 +0530

    feat(day11): add RestaurantService interface and implementation classes for BurgerKing and Dominoz

---
+++ Day 11/Dominoz.java
@@ -0,0 +1,5 @@
+package Day 11;
+
+public class Dominoz {
+    
+}
---
+++ Day 11/RestrauntService.java
@@ -0,0 +1,12 @@
+package Day 11;
+
+public interface RestrauntService {
+    String Restrauntype = "Gourmet";
+
+    void acceptOrder(String CustomerName);
+
+    void PrepareFood();
+
+    void DeliverFood();
+
+}
