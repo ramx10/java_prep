1b135cf3ad6b819bce8c7e39f550bfd2c90b35f9
ramx10 <Shipalkarram@gmail.com>
2026-06-19 11:56:15 +0530

    feat: implement Customer entity updates and CRUD APIs

--- SpringBoot/demo/src/main/java/com/example/demo/controller/CustomerController.java
+++ SpringBoot/demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customers")
@@ -26,6 +27,40 @@ public class CustomerController {
             return  service.SaveCustomer(cust);
         }
 
+        @GetMapping("/{id}")
+        Optional<Customer> getCustomerbyId(@PathVariable Long id){
+                return  service.getCustomerById(id);
+        }
+
+        @GetMapping("/email/{email}")
+        Customer getCustomerByEmail(@PathVariable String email){
+            return service.getCustomerByEmail(email);
+        }
+
+        @GetMapping("/city/{city}")
+        List<Customer> getCustomerByCity(@PathVariable String city){
+            return service.getCustomerByCity(city);
+        }
+
+        @GetMapping("/age/{age}")
+        List<Customer> getCustomerByAge(@PathVariable int age){
+            return service.getCustomerByAge(age);
+        }
+
+        @PostMapping("/list")
+        List<Customer> SaveCustomerByAList(@RequestBody List<Customer> cust){
+            return service.saveCustomerList(cust);
+        }
+
+        @PutMapping("/{Id}")
+        Customer UpdateCustomerById(@PathVariable Long Id , @RequestBody Customer cust){
+            return service.UpdateCustomer(Id,cust);
+        }
+
+        @DeleteMapping("/{Id}")
+        void DeleteCustomerById(@PathVariable  Long Id){
+            service.deleteCustomer(Id);
+        }
 
 
 }
